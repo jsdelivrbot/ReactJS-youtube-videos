04d6d60e599c41e5c31fba9ffa62d83ce91dff09
Kelvin Mbewe <keokeil@hotmail.com>
2017-05-16 09:36:33 +0200

    refactoring functional Components to class Components :heart_eyes::heart_eyes:

--- README.md
+++ README.md
@@ -1,12 +1,11 @@
 # YouTube Clone
 ### **Getting started**
-### Technologies Used: 
-#### HTML5, CSS3, React.js
+### Technologies Used:
+#### HTML5, CSS3, Bootstrap 4.0, React.js, RESTful Api, JSON
  - Run  :  `git clone https://github.com/getwebem/youTubeClone.git`
  - Run  :  `cd youTubeClone`
  - Run  :  `npm install`
  - Run  :  `npm start`
  - Browser :  `http://localhost:8080`
- 
+
  ### Used Boilerplate: https://github.com/StephenGrider/ReduxSimpleStarter.git
-  
--- src/index.js
+++ src/index.js
@@ -1,18 +1,25 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-
+import React, {Component} from 'react';
+import ReactDOM  from 'react-dom';
 import SearchBar from './components/search_bar';
-
-
-
+import YTSearch  from 'youtube-api-search';
 const API_KEY = 'AIzaSyAnV4WzryaY29Kzqmgtc1ot2uKU9CoXWoo';
 
-const App = () => {
+class App extends Component {
+  constructor(props){
+  super(props);
+  this.state = {videos: [] };
+
+  YTSearch({key: API_KEY, term: 'surfboards'}, (videos) => {
+    this.setState({videos});
+  });
+  }
+
+render(){
   return (
     <div>
       <SearchBar />
     </div>
   );
+ }
 }
-
 ReactDOM.render(<App />, document.querySelector('.container'));
